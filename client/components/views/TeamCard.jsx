074c46d6e197db515e15d8bfa21973302a3503ae
Avrahm <avrahmk@me.com>
2022-05-08 22:02:53 -0400

    updated teamcard, photo component design

--- client/components/views/TeamCard.jsx
+++ client/components/views/TeamCard.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useWallet } from "../../context/WalletProvider";
+import { Photo } from "../Photo";
 import { ManageTeam } from "./ManageTeam";
 
 export const TeamsCard = ({ team, teamObject = null }) => {
     const { user } = useWallet();
     const [manageTeamModal, toggleManageTeamModal] = useState(false);
-    const isTeamMember = false;
-    const isAdmin = false;
+    let isTeamMember = false;
+    let isAdmin = false;
 
     if (user) {
         const { username } = user.attributes ? user.attributes : "";
@@ -16,31 +17,18 @@ export const TeamsCard = ({ team, teamObject = null }) => {
 
     return (
         <div className="flex flex-col my-4 w-full justify-center items-start border-2 border-emerald-400 p-2">
-            <div className="flex flex-row ">
-                <div className="flex flex-col w-full items-center justify-center p-2">
-                    <span className="p-2 font-bold">Team Name:</span>
-                    <span> {team.teamName}</span>
-                    <span className="p-2 font-bold">Description:</span>
-                    <span> {team.teamDescription}</span>
+            <div className="flex flex-row w-full">
+                <div className="flex flex-col w-1/2 items-center justify-center p-2">
+                    <Photo src={team.teamPhoto} alt={team.teamDisplayName} size="sm" type="team" isLoading={false} />
+                    <span>{team.teamName}</span>
+                    <span>{team.teamPOS ? `${team.teamPOS}% ` : "100%"} POS</span>
                 </div>
-                <div className="flex flex-col w-full items-center p-2">
+                <div className="flex flex-col w-1/2 items-center p-2">
                     <div className="flex flex-col justify-center items-center">
                         <span className="p-2 font-bold">Record:</span>
                         <span>
                             {team.teamWins} Wins - {team.teamLosses} Losses
                         </span>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <span className="p-2 font-bold">Winnings:</span>
-                        <span>{team.teamWinnings} VYBES</span>
-                    </div>
-                </div>
-                <div className="flex flex-col w-full items-center p-2">
-                    <div className="flex flex-col justify-center items-center">
-                        <span className="p-2 font-bold">Sportsmanship:</span>
-                        <span>{team.teamPOS ? `${team.teamPOS}% ` : "100%"}</span>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
                         <span className="p-2 font-bold">Sport Preferences:</span>
                         <ul>
                             {team.teamSportsPreferences &&
@@ -59,7 +47,9 @@ export const TeamsCard = ({ team, teamObject = null }) => {
                         Manage Team
                     </button>
                 )}
-                <button className="px-2 py-1 my-4 bg-blue-400 rounded-full">View Team</button>
+                <a href={`/team/${teamObject.id}`} className="px-2 py-1 my-4 bg-blue-400 rounded-full">
+                    View Team
+                </a>
             </div>
             <ManageTeam user={user} team={team} teamObject={teamObject} toggleModal={toggleManageTeamModal} modalView={manageTeamModal} />
         </div>
