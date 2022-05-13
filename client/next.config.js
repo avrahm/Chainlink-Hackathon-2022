3da3ca56bd6ed7a832304ef3db02ce6287b5a8a2
Avrahm <avrahmk@me.com>
2022-05-12 22:29:44 -0400

    new photo server from moralis

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     images: {
-        domains: ["3bylwe5lrewo.usemoralis.com"],
+        domains: ["cxt0kq8qfq9t.usemoralis.com"],
     },
 };
 
