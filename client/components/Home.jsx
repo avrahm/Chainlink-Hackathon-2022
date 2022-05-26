99c3dffc0a4df64e96cecc3906fcd8ee1cdbb64a
jaymesC <clementjaymes@gmail.com>
2022-05-25 22:40:57 -0700

    updated homw page styles

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,11 +1,49 @@
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export const Home = () => {
-    return (
-        <div className={styles.container}>
-            <h1>Home</h1>
-
-            <div>What is SportsVybe?</div>
+  return (
+    <div>
+      <div className="flex items-center justify-center">
+        <div>
+          <h1 className="text-7xl mb-5 font-semibold">
+            <span className="text-green-500 pt-10">Sports</span>Vybe
+          </h1>
+          <h4 className="text-2xl text-gray-500">
+            Connecting People to Recreational Sports
+          </h4>
+          <p className="text-lg pt-3 text-gray-500">
+            The easiest way to find pickup games, sport leagues, & recreational{" "}
+            <br />
+            facilities - All in the palm of your hand!
+          </p>
+          <h4 className="pt-5 font-serif font-thin text-gray-500 text-lg">
+            For Both Web and Mobile Devices
+          </h4>
         </div>
-    );
+
+        <div>
+          <img
+            src="/heroSecImg.png"
+            alt="SportsVybe header"
+            width={600}
+            height={800}
+          />
+        </div>
+      </div>
+      <div>
+        <h3 className="text-3xl text-center mt-6">
+          OUR <span className="text-green-500">MISSION</span>:
+        </h3>
+        <h2 className=" text-center mt-2">
+          Our Mission is to improve the health in our community through fitness
+          and sociability.
+        </h2>
+          </div>
+          
+          <div>
+              <h2>How SportsVybe Works:</h2>
+          </div>
+    </div>
+  );
 };
--- client/components/Layout/Navbar.jsx
+++ client/components/Layout/Navbar.jsx
@@ -4,56 +4,76 @@ import { useWallet } from "../../context/WalletProvider";
 import styles from "../../styles/Home.module.css";
 
 export default function Navbar() {
-    const { connectWallet, wallet, signOutWallet, isAuthenticating, isAuthenticated } = useWallet();
+  const {
+    connectWallet,
+    wallet,
+    signOutWallet,
+    isAuthenticating,
+    isAuthenticated,
+  } = useWallet();
 
-    return (
-        <div className={styles.nav}>
-            <div className="px-4 items-center">
-                <Image src="/logo_blackvybe.png" alt="SportsVybe Logo" width={125} height={25} />
-            </div>
-            <div className="px-4">
-                <Link href="/" className="px-2">
-                    Home
-                </Link>
-            </div>
-            <div className="px-4">
-                <Link href="/teams" className="px-2">
-                    Teams
-                </Link>
-            </div>
-            {isAuthenticated && (
-                <>
-                    <div className="px-4">
-                        <Link href="/profile" className="px-2">
-                            Profile
-                        </Link>
-                    </div>
-                    <div className="px-4">
-                        <Link href="/challenges" className="px-2">
-                            Challenges
-                        </Link>
-                    </div>
-                </>
-            )}
-            <div className="px-4">
-                {wallet ? (
-                    <button
-                        disabled={isAuthenticating}
-                        className="rounded-full bg-green-200 px-2 py-1 mx-4  disabled:bg-gray-400"
-                        onClick={() => signOutWallet()}
-                    >
-                        Disconnect {wallet.substring(0, 5)}
-                    </button>
-                ) : (
-                    <button
-                        disabled={isAuthenticating}
-                        className="rounded-full bg-green-200 px-2 py-1 mx-4 disabled:bg-gray-400"
-                        onClick={() => connectWallet()}
-                    >
-                        Connect Wallet
-                    </button>
-                )}
-            </div>
+  return (
+    <div className={styles.nav}>
+      <div className="px-4 items-center justify-center cursor-pointer">
+        <Link href="/">
+          <Image
+            src="/logo_blackvybe.png"
+            alt="SportsVybe Logo"
+            width={125}
+            height={25}
+          />
+        </Link>
+      </div>
+      <div className="flex">
+        <div className="px-4 hover:text-green-600">
+          <Link href="/" className="px-2">
+            Home
+          </Link>
         </div>
-    );
+        <div className="px-4 hover:text-green-600">
+          <Link href="/teams" className="px-2">
+            Teams
+          </Link>
+        </div>
+        <div className="px-4 hover:text-green-600">
+          <Link href="/event" className="px-2">
+            Events
+          </Link>
+        </div>
+        {isAuthenticated && (
+          <>
+            <div className="px-4 hover:text-green-600">
+              <Link href="/profile" className="px-2">
+                Profile
+              </Link>
+            </div>
+            <div className="px-4 hover:text-green-600">
+              <Link href="/challenges" className="px-2">
+                Challenges
+              </Link>
+            </div>
+          </>
+        )}
+        <div className="px-4 ">
+          {wallet ? (
+            <button
+              disabled={isAuthenticating}
+              className="rounded-full bg-green-200 px-2 py-1 mx-4  disabled:bg-gray-400 hover:bg-green-400"
+              onClick={() => signOutWallet()}
+            >
+              Disconnect {wallet.substring(0, 5)}
+            </button>
+          ) : (
+            <button
+              disabled={isAuthenticating}
+              className="rounded-full bg-green-200 px-3 py-1 mx-4 disabled:bg-gray-400 hover:bg-green-400 "
+              onClick={() => connectWallet()}
+            >
+              Connect Wallet
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
