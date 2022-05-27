a3f038d409ec571a81798a8ec6171db03107e646
Avrahm <avrahmk@me.com>
2022-05-27 18:36:41 -0400

    updated nav w/ hamburger menu. fix images config

--- client/components/Layout/Navbar.jsx
+++ client/components/Layout/Navbar.jsx
@@ -1,79 +1,83 @@
 import Image from "next/image";
-import Link from "next/link";
+import { default as Link } from "next/link";
+import { useState } from "react";
 import { useWallet } from "../../context/WalletProvider";
-import styles from "../../styles/Home.module.css";
 
 export default function Navbar() {
-  const {
-    connectWallet,
-    wallet,
-    signOutWallet,
-    isAuthenticating,
-    isAuthenticated,
-  } = useWallet();
+    const { connectWallet, wallet, signOutWallet, isAuthenticating, isAuthenticated } = useWallet();
 
-  return (
-    <div className={styles.nav}>
-      <div className="px-4 items-center justify-center cursor-pointer">
-        <Link href="/">
-          <Image
-            src="/logo_blackvybe.png"
-            alt="SportsVybe Logo"
-            width={125}
-            height={25}
-          />
-        </Link>
-      </div>
-      <div className="flex">
-        <div className="px-4 hover:text-green-600">
-          <Link href="/" className="px-2">
-            Home
-          </Link>
-        </div>
-        <div className="px-4 hover:text-green-600">
-          <Link href="/teams" className="px-2">
-            Teams
-          </Link>
-        </div>
-        <div className="px-4 hover:text-green-600">
-          <Link href="/event" className="px-2">
-            Events
-          </Link>
-        </div>
-        {isAuthenticated && (
-          <>
-            <div className="px-4 hover:text-green-600">
-              <Link href="/profile" className="px-2">
-                Profile
-              </Link>
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <nav className="md:px-4 md:flex md:justify-between md:items-center">
+            <div className="flex items-center justify-between px-4">
+                <div className="p-2">
+                    <Link href="/">
+                        <Image src="/logo_blackvybe.png" alt="SportsVybe Logo" width={125} height={25} />
+                    </Link>
+                </div>
+                <div className="md:hidden p-2">
+                    <button onClick={() => setIsOpen(!isOpen)} type="button" className="block text-[#2546bd] hover:[#2546bd]">
+                        <svg className="h-6 w-6 fill-current" viewBox="0 0 100 80">
+                            <rect width="100" height="20"></rect>
+                            <rect y="30" width="100" height="20"></rect>
+                            <rect y="60" width="100" height="20"></rect>
+                        </svg>
+                    </button>
+                </div>
             </div>
-            <div className="px-4 hover:text-green-600">
-              <Link href="/challenges" className="px-2">
-                Challenges
-              </Link>
+            <div className={`px-4 pt-2 pb-4 md:pb-0 md:flex md:p-0 ${isOpen ? "block" : "hidden"}`}>
+                <div className="md:flex list-none flex-row justify-between items-center flex-initial">
+                    <div className="block rounded px-2 py-1 mx-4 cursor-pointer hover:text-green-600">
+                        <Link href="/" className="px-2">
+                            Home
+                        </Link>
+                    </div>
+                    <div className="block rounded px-2 py-1 mx-4 cursor-pointer hover:text-green-600">
+                        <Link href="/teams" className="px-2">
+                            Teams
+                        </Link>
+                    </div>
+                    <div className="block rounded px-2 py-1 mx-4 cursor-pointer hover:text-green-600">
+                        <Link href="/event" className="px-2">
+                            Events
+                        </Link>
+                    </div>
+                    {isAuthenticated && (
+                        <>
+                            <div className="block rounded px-2 py-1 mx-4 cursor-pointer hover:text-green-600">
+                                <Link href="/profile" className="px-2">
+                                    Profile
+                                </Link>
+                            </div>
+                            <div className="block rounded px-2 py-1 mx-4 cursor-pointer hover:text-green-600">
+                                <Link href="/challenges" className="px-2">
+                                    Challenges
+                                </Link>
+                            </div>
+                        </>
+                    )}
+                </div>
+                <div className="px-4 ">
+                    {wallet ? (
+                        <button
+                            disabled={isAuthenticating}
+                            className="block rounded-full cursor-pointer px-6 py-2  mx-2 bg-green-200  disabled:bg-gray-400 hover:bg-green-400"
+                            onClick={() => signOutWallet()}
+                        >
+                            Disconnect {wallet.substring(0, 5)}
+                        </button>
+                    ) : (
+                        <button
+                            disabled={isAuthenticating}
+                            className="block rounded-full cursor-pointer px-6 py-2  mx-2 bg-green-200   disabled:bg-gray-400 hover:bg-green-400"
+                            onClick={() => connectWallet()}
+                        >
+                            Connect Wallet
+                        </button>
+                    )}
+                </div>
             </div>
-          </>
-        )}
-        <div className="px-4 ">
-          {wallet ? (
-            <button
-              disabled={isAuthenticating}
-              className="rounded-full bg-green-200 px-2 py-1 mx-4  disabled:bg-gray-400 hover:bg-green-400"
-              onClick={() => signOutWallet()}
-            >
-              Disconnect {wallet.substring(0, 5)}
-            </button>
-          ) : (
-            <button
-              disabled={isAuthenticating}
-              className="rounded-full bg-green-200 px-3 py-1 mx-4 disabled:bg-gray-400 hover:bg-green-400 "
-              onClick={() => connectWallet()}
-            >
-              Connect Wallet
-            </button>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+        </nav>
+    );
 }
