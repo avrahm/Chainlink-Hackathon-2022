3809304de8c54ec4e6b8f78c7df2f4d7471baed1
Avrahm <avrahmk@me.com>
2022-05-28 23:31:21 -0400

    teampage

--- client/components/views/ManageTeam.jsx
+++ client/components/views/ManageTeam.jsx
@@ -80,118 +80,99 @@ export const ManageTeam = ({ user, team = false, toggleModal, modalView, createN
     };
 
     return (
-      <Modal open={modalView} onClose={() => toggleModal(false)}>
-        <div className="flex flex-col border-2 border-green-100 p-4 items-center">
-          <div> {createNewTeam ? "Create Team" : "Manage Team"}</div>
+        <Modal open={modalView} onClose={() => toggleModal(false)}>
+            <div className="flex flex-col border-2 border-green-100 p-4 items-center">
+                <div> {createNewTeam ? "Create Team" : "Manage Team"}</div>
 
-          {/* {teamError && <span className="py-2">Team Update Error:{teamError}</span>} */}
-          {/* {error && <span className="py-2">Upload Error: {error}</span>} */}
-          {isContractLoading && !contractMessage ? (
-            <span className="my-3 px-2 py-1 ">...Minting</span>
-          ) : (
-            <>
-              <div className="flex flex-row w-full">
-                <div className="w-1/2 p-2">
-                  <span className="h-[60px] my-1 flex justify-end items-center">
-                    Team Name:
-                  </span>
+                {/* {teamError && <span className="py-2">Team Update Error:{teamError}</span>} */}
+                {/* {error && <span className="py-2">Upload Error: {error}</span>} */}
+                {isContractLoading && !contractMessage ? (
+                    <span className="my-3 px-2 py-1 ">...Minting</span>
+                ) : (
+                    <>
+                        <div className="flex flex-row w-full">
+                            <div className="w-1/2 p-2">
+                                <span className="h-[60px] my-1 flex justify-end items-center">Team Name:</span>
 
-                  <span className="h-[60px] my-1 flex justify-end items-center">
-                    Description:
-                  </span>
+                                <span className="h-[60px] my-1 flex justify-end items-center">Description:</span>
 
-                  <span className="h-[120px] my-1 flex justify-end items-center">
-                    Sports Preferences:
-                  </span>
+                                <span className="h-[120px] my-1 flex justify-end items-center">Sports Preferences:</span>
 
-                  <span className="h-[60px] my-1 flex justify-end items-center">
-                    Team Photo:
-                  </span>
-                </div>
-                <div className="w-1/2 p-2">
-                  <span className="h-[60px] my-1 flex justify-start items-center">
-                    <input
-                      value={teamName}
-                      className="m-2 px-2 py-1 rounded bg-gray-300 outline-green-400"
-                      onChange={(e) => setTeamName(e.target.value)}
-                    />
-                  </span>
-                  <span className="h-[60px] my-1 flex justify-start items-center">
-                    <textarea
-                      id="teamDescription"
-                      className="m-2 px-2 py-1 rounded bg-gray-300 outline-green-400"
-                      placeholder="Enter team description..."
-                      rows="3"
-                      value={teamDescription}
-                      onChange={(e) => setTeamDescription(e.target.value)}
-                    />
-                  </span>
-                  <span className="h-[120px] my-1 flex justify-start items-center flex-wrap">
-                    {sports.sort().map((sport, i) => {
-                      const isChecked = teamSportsPreferences.includes(sport);
-                      return (
+                                <span className="h-[60px] my-1 flex justify-end items-center">Team Photo:</span>
+                            </div>
+                            <div className="w-1/2 p-2">
+                                <span className="h-[60px] my-1 flex justify-start items-center">
+                                    <input
+                                        value={teamName}
+                                        className="m-2 px-2 py-1 rounded bg-gray-300 outline-green-400"
+                                        onChange={(e) => setTeamName(e.target.value)}
+                                    />
+                                </span>
+                                <span className="h-[60px] my-1 flex justify-start items-center">
+                                    <textarea
+                                        id="teamDescription"
+                                        className="m-2 px-2 py-1 rounded bg-gray-300 outline-green-400"
+                                        placeholder="Enter team description..."
+                                        rows="3"
+                                        value={teamDescription}
+                                        onChange={(e) => setTeamDescription(e.target.value)}
+                                    />
+                                </span>
+                                <span className="h-[120px] my-1 flex justify-start items-center flex-wrap">
+                                    {sports.sort().map((sport, i) => {
+                                        const isChecked = teamSportsPreferences.includes(sport);
+                                        return (
+                                            <button
+                                                key={i}
+                                                className={`m-2 px-2 py-1 rounded text-sm  ${isChecked ? "bg-green-100" : "bg-gray-300"}`}
+                                                onClick={() => handleSportsPreferences(sport, isChecked)}
+                                            >
+                                                {sport}
+                                            </button>
+                                        );
+                                    })}
+                                </span>
+                                <span className="h-[60px] my-1 flex justify-start items-center">
+                                    {/* {file && <Image src={file.preview} width="150" height="150" />} */}
+                                    <input
+                                        accept="image/x-png,image/gif,image/jpeg"
+                                        type="file"
+                                        onChange={(e) => setFile(e.target.files[0])}
+                                        className="m-2 px-2 py-1 rounded bg-gray-300 outline-green-400"
+                                    />
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="p-2">
+                            <div className="flex flex-row justify-center">
+                                <label className="mx-3">{isTeamActive ? "Active" : "Inactive"}</label>
+                                <div
+                                    className="md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer"
+                                    onClick={() => {
+                                        setIsTeamActive(!isTeamActive);
+                                    }}
+                                >
+                                    <div
+                                        className={
+                                            "bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform" + (isTeamActive ? toggleClass : null)
+                                        }
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+
                         <button
-                          key={i}
-                          className={`m-2 px-2 py-1 rounded text-sm  ${
-                            isChecked ? "bg-green-100" : "bg-gray-300"
-                          }`}
-                          onClick={() =>
-                            handleSportsPreferences(sport, isChecked)
-                          }
+                            disabled={isContractLoading}
+                            className="my-3 px-2 py-1 bg-green-300 rounded-full disabled:bg-slate-300"
+                            onClick={() => handleSubmit()}
                         >
-                          {sport}
+                            {createNewTeam ? "Create Team" : "Update Team"}
                         </button>
-                      );
-                    })}
-                  </span>
-                  <span className="h-[60px] my-1 flex justify-start items-center">
-                    {/* {file && <Image src={file.preview} width="150" height="150" />} */}
-                    <input
-                      accept="image/x-png,image/gif,image/jpeg"
-                      type="file"
-                      onChange={(e) => setFile(e.target.files[0])}
-                      className="m-2 px-2 py-1 rounded bg-gray-300 outline-green-400"
-                    />
-                  </span>
-                </div>
-              </div>
-
-              <div className="p-2">
-                <div className="flex flex-row justify-center">
-                  <label className="mx-3">
-                    {isTeamActive ? "Active" : "Inactive"}
-                  </label>
-                  <div
-                    className="md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer"
-                    onClick={() => {
-                      setIsTeamActive(!isTeamActive);
-                    }}
-                  >
-                    <div
-                      className={
-                        "bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform" +
-                        (isTeamActive ? toggleClass : null)
-                      }
-                    ></div>
-                  </div>
-                </div>
-              </div>
-
-              <button
-                disabled={isContractLoading}
-                className="my-3 px-2 py-1 bg-green-300 rounded-full disabled:bg-slate-300"
-                onClick={() => handleSubmit()}
-              >
-                {createNewTeam ? "Create Team" : "Update Team"}
-              </button>
-              {contractMessage && !isContractLoading && (
-                <Toast type={contractMessage.statusColor}>
-                  {contractMessage.message}
-                </Toast>
-              )}
-            </>
-          )}
-        </div>
-      </Modal>
+                        {contractMessage && !isContractLoading && <Toast type={contractMessage.statusColor}>{contractMessage.message}</Toast>}
+                    </>
+                )}
+            </div>
+        </Modal>
     );
 };
